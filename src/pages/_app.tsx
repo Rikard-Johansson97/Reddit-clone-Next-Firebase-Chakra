ef1ae62667abb81f6d293b697187b8ab29855098
Rikard Johansson <Rikard.com@hotmail.com>
2023-02-20 19:48:12 +0100

    Init

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,16 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import { theme } from "@/chakra/theme";
+import store from "@/store/store";
+import "@/styles/globals.css";
+import { ChakraProvider } from "@chakra-ui/react";
+import type { AppProps } from "next/app";
+import { Provider } from "react-redux";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Provider store={store}>
+      <ChakraProvider theme={theme}>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </Provider>
+  );
 }
