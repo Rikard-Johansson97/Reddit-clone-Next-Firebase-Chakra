1fb1de864bbc41957005f19704e0c38c7c0fd78d
Rikard Johansson <106700975+Rikard-Johansson97@users.noreply.github.com>
2023-02-25 12:58:00 +0100

    Added not found community component

--- src/pages/r/[communityId]/index.tsx
+++ src/pages/r/[communityId]/index.tsx
@@ -1,3 +1,4 @@
+import NotFound from "@/components/Community/NotFound";
 import { firestore } from "@/firebase/clientApp";
 import { Community } from "@/store/communitiesSlice";
 import { doc, getDoc } from "firebase/firestore";
@@ -11,6 +12,10 @@ interface CommunityPageProps {
 
 const CommunityPage: FC<CommunityPageProps> = ({ communityData }) => {
   console.log("Community data", communityData);
+
+  if (!communityData) {
+    return <NotFound />;
+  }
   return <div>{communityData.id}</div>;
 };
 
@@ -25,9 +30,11 @@ export async function getServerSideProps(context: GetServerSidePropsContext) {
 
     return {
       props: {
-        communityData: JSON.parse(
-          safeJsonStringify({ id: communityDoc.id, ...communityDoc.data() })
-        ),
+        communityData: communityDoc.exists()
+          ? JSON.parse(
+              safeJsonStringify({ id: communityDoc.id, ...communityDoc.data() })
+            )
+          : "",
       },
     };
   } catch (error) {
